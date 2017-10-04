1c29a8798c6aecb2350ff6c6946b81cfef7ada6c
Travis Greenman <cartman-2000@cartman-2000.net>
2017-10-03 23:40:48 -0700

    Alpha: add config vars to plugin.

--- AntiGrief.cs
+++ AntiGrief.cs
@@ -30,8 +30,10 @@ namespace AntiGrief
 
             Logger.LogWarning("Starting anti grief modification run.");
             BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            bool shouldUpdateCount;
             for (int i = 0; i < AssetList.Length; i++)
             {
+                shouldUpdateCount = false;
                 Asset asset = AssetList[i];
                 bool shouldSkip = false;
                 for (int s = 0; s < Configuration.Instance.SkipItemIDs.Count; s++)
@@ -47,7 +49,22 @@ namespace AntiGrief
                 if (asset is ItemWeaponAsset)
                 {
                     ItemWeaponAsset weaponAsset = asset as ItemWeaponAsset;
-                    if (weaponAsset.barricadeDamage > 0 || weaponAsset.structureDamage > 0 || weaponAsset.vehicleDamage > 0)
+                    if (weaponAsset.barricadeDamage > 0 && Configuration.Instance.NegateBarricadeDamage)
+                    {
+                        weaponAsset.barricadeDamage = 0;
+                        shouldUpdateCount = true;
+                    }
+                    if (weaponAsset.structureDamage > 0 && Configuration.Instance.NegateStructureDamage)
+                    {
+                        weaponAsset.structureDamage = 0;
+                        shouldUpdateCount = true;
+                    }
+                    if (weaponAsset.vehicleDamage > 0 && Configuration.Instance.NegateVehicleDamage)
+                    {
+                        weaponAsset.vehicleDamage = 0;
+                        shouldUpdateCount = true;
+                    }
+                    if (shouldUpdateCount)
                     {
                         if (weaponAsset is ItemGunAsset)
                             gunsModified++;
@@ -55,43 +72,70 @@ namespace AntiGrief
                             meleesModified++;
                         if (weaponAsset is ItemThrowableAsset)
                             throwablesModified++;
-                        weaponAsset.barricadeDamage = 0;
-                        weaponAsset.structureDamage = 0;
-                        weaponAsset.vehicleDamage = 0;
                     }
                 }
                 else if (asset is ItemTrapAsset)
                 {
                     ItemTrapAsset trapAsset = asset as ItemTrapAsset;
-                    if (trapAsset.barricadeDamage > 0 || trapAsset.structureDamage > 0 || trapAsset.vehicleDamage > 0)
+                    if (trapAsset.barricadeDamage > 0 && Configuration.Instance.NegateBarricadeDamage)
                     {
-                        trapsModified++;
                         trapAsset.GetType().GetField("_barricadeDamage", bindingFlags).SetValue(trapAsset, 0);
-                        trapAsset.GetType().GetField("_structureDamage", bindingFlags).SetValue(trapAsset, 0);
-                        trapAsset.GetType().GetField("_vehicleDamage", bindingFlags).SetValue(trapAsset, 0);
+                        shouldUpdateCount = true;
                     }
+                    if (trapAsset.structureDamage > 0 && Configuration.Instance.NegateStructureDamage)
+                    {
+                        trapAsset.GetType().GetField("_structureDamage", bindingFlags).SetValue(trapAsset, 0);
+                        shouldUpdateCount = true;
+                    }
+                    if (trapAsset.structureDamage > 0 && Configuration.Instance.NegateVehicleDamage)
+                    {
+                        trapAsset.GetType().GetField("_vehicleDamage", bindingFlags).SetValue(trapAsset, 0);
+                        shouldUpdateCount = true;
+                    }
+                    if (shouldUpdateCount)
+                        trapsModified++;
                 }
                 else if (asset is ItemChargeAsset)
                 {
                     ItemChargeAsset chargeAsset = asset as ItemChargeAsset;
-                    if (chargeAsset.barricadeDamage > 0 || chargeAsset.structureDamage > 0 || chargeAsset.vehicleDamage > 0)
+                    if (chargeAsset.barricadeDamage > 0 && Configuration.Instance.NegateBarricadeDamage)
                     {
-                        chargesModified++;
                         chargeAsset.GetType().GetField("_barricadeDamage", bindingFlags).SetValue(chargeAsset, 0);
-                        chargeAsset.GetType().GetField("_structureDamage", bindingFlags).SetValue(chargeAsset, 0);
-                        chargeAsset.GetType().GetField("_vehicleDamage", bindingFlags).SetValue(chargeAsset, 0);
+                        shouldUpdateCount = true;
                     }
+                    if (chargeAsset.structureDamage > 0 && Configuration.Instance.NegateStructureDamage)
+                    {
+                        chargeAsset.GetType().GetField("_structureDamage", bindingFlags).SetValue(chargeAsset, 0);
+                        shouldUpdateCount = true;
+                    }
+                    if (chargeAsset.vehicleDamage > 0 && Configuration.Instance.NegateVehicleDamage)
+                    {
+                        chargeAsset.GetType().GetField("_vehicleDamage", bindingFlags).SetValue(chargeAsset, 0);
+                        shouldUpdateCount = true;
+                    }
+                    if (shouldUpdateCount)
+                        chargesModified++;
                 }
                 else if (asset is ItemMagazineAsset)
                 {
                     ItemMagazineAsset magAsset = asset as ItemMagazineAsset;
-                    if (magAsset.barricadeDamage > 0 || magAsset.structureDamage > 0 || magAsset.vehicleDamage > 0)
+                    if (magAsset.barricadeDamage > 0 && Configuration.Instance.NegateBarricadeDamage)
                     {
-                        magsModified++;
                         magAsset.GetType().GetField("_barricadeDamage", bindingFlags).SetValue(magAsset, 0);
-                        magAsset.GetType().GetField("_structureDamage", bindingFlags).SetValue(magAsset, 0);
-                        magAsset.GetType().GetField("_vehicleDamage", bindingFlags).SetValue(magAsset, 0);
+                        shouldUpdateCount = true;
                     }
+                    if (magAsset.structureDamage > 0 && Configuration.Instance.NegateStructureDamage)
+                    {
+                        magAsset.GetType().GetField("_structureDamage", bindingFlags).SetValue(magAsset, 0);
+                        shouldUpdateCount = true;
+                    }
+                    if (magAsset.vehicleDamage > 0 && Configuration.Instance.NegateVehicleDamage)
+                    {
+                        magAsset.GetType().GetField("_vehicleDamage", bindingFlags).SetValue(magAsset, 0);
+                        shouldUpdateCount = true;
+                    }
+                    if (shouldUpdateCount)
+                        magsModified++;
                 }
             }
 
@@ -99,6 +143,7 @@ namespace AntiGrief
             Asset[] vehicleList = Assets.find(EAssetType.VEHICLE);
             for (int v = 0; v < vehicleList.Length; v++)
             {
+                shouldUpdateCount = false;
                 Asset asset = vehicleList[v];
                 bool shouldSkip = false;
                 for (int i = 0; i < Configuration.Instance.SkipVehicleIDs.Count; i++)
@@ -113,12 +158,18 @@ namespace AntiGrief
                     continue;
 
                 VehicleAsset vAsset = asset as VehicleAsset;
-                if (!vAsset.isVulnerable || !vAsset.canTiresBeDamaged)
+                if (!vAsset.isVulnerable && Configuration.Instance.MakeVehiclesInvulnerable)
                 {
-                    vehiclesModified++;
                     vAsset.GetType().GetField("_isVulnerable", bindingFlags).SetValue(vAsset, false);
-                    vAsset.GetType().GetProperty("canTiresBeDamaged", bindingFlags | BindingFlags.Public).SetValue(vAsset, false, null);
+                    shouldUpdateCount = true;
                 }
+                if (!vAsset.canTiresBeDamaged && Configuration.Instance.MakeTiresInvulnerable)
+                {
+                    vAsset.GetType().GetProperty("canTiresBeDamaged", bindingFlags | BindingFlags.Public).SetValue(vAsset, false, null);
+                    shouldUpdateCount = true;
+                }
+                if (shouldUpdateCount)
+                    vehiclesModified++;
             }
             Logger.LogWarning(string.Format("Finished modification run, counts of bundles modified: Guns: {0}, Mags: {6}, Melee: {1}, Throwables: {2}, Traps: {3}, Charges: {4}, Vehicles: {5}.", gunsModified, meleesModified, throwablesModified, trapsModified, chargesModified, vehiclesModified, magsModified));
         }
